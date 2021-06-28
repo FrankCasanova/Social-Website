114cd6934cf0d7af49b3155ec40d0c27d08069aa
Frank Casanova <frankcasanova.info@gmail.com>
2021-06-28 14:54:46 +0200

    using contenttype framework

--- actions/models.py
+++ actions/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType  # 201
+from django.contrib.contenttypes.fields import GenericForeignKey  # 201
 
 # Create your models here.
 
@@ -10,8 +12,17 @@ class Action(models.Model):
                              db_index=True,
                              on_delete=models.CASCADE)
     verb = models.CharField(max_length=255)
+    target_ct = models.ForeignKey(ContentType,
+                                  blank=True,
+                                  null=True,
+                                  related_name='target_obj',
+                                  on_delete=models.CASCADE)
+    target_id = models.PositiveBigIntegerField(null=True,
+                                               blank=True,
+                                               db_index=True)
+    target = GenericForeignKey('target_ct', 'target_id')
     created = models.DateTimeField(auto_now_add=True,
                                    db_index=True)
 
     class Meta:
-        ordering = ('-created')
+        ordering = ('-created',)
