095d2cca16a4cf9cda7e7bffe4a92eb88ba8b550
Frank Casanova <frankcasanova.info@gmail.com>
2021-06-30 13:55:45 +0200

    project finished v1.

--- README.md
+++ README.md
@@ -1,7 +1,44 @@
 # Social-Website
 A social web site
+------------------PROJECT IN DEVELOPMENT--------------------
+
+I hope can deploy this soon, but is some tedious
+
+This project is just for training concepts like OAuth and complex functions with Redis
+
+------------------------
+I want dockerize all
+fix some bugs
+TDD has not been aplied, I want to cover all project 
+and then create a deploy versión
+----------------------- 
+
+for WSL:
+sudo service postgresql restart
+sudo service redis-server start
+source venv/bin/activate
 
 for test with certificate host use:
 
 python manage.py runserver_plus --cert-file cert.crt
+
+main page:
+https://mysite.com:8000/account/
+
+
+-A follow system using many-to-many relationships with an 
+intermediary model.
+
+-Activity stream using generic relations and 
+optimized QuerySets to retrieve related objects.
+
+-Django signals, a signal receiver function to denormalize 
+related object counts.
+
+-Application configuration classes, to load signal handlers.
+
+-I used Redis in this project to store item views, 
+and built an image ranking with Redis.
+
+------------------PROJECT IN DEVELOPMENT--------------------
 
--- images/views.py
+++ images/views.py
@@ -24,7 +24,6 @@ r = redis.Redis(host=settings.REDIS_HOST,
 
 @login_required
 def image_create(request):  # 5-8
-
     if request.method == 'POST':
         #form is set
         form = ImageCreateForm(data=request.POST)
@@ -56,6 +55,8 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     # increment total image views by 1
     total_views = r.incr(f'image:{image.id}:views')
+    # increment image ranking by 1
+    r.zincrby('image_ranking', 1, image.id)
     return render(request,
                   'images/image/detail.html',
                   {'section': 'images',
@@ -108,3 +109,18 @@ def image_list(request):
     return render(request,
                   'images/image/list.html',
                   {'section': 'images', 'images': images})
+
+
+@login_required
+def image_ranking(request):
+    # get image ranking dictionary
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # get most viewed images
+    most_viewed = list(Image.objects.filter(
+        id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request,
+                  'images/image/ranking.html',
+                  {'section': 'images',
+                   'most_viewed': most_viewed})
