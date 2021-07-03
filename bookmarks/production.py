ccf0b90b28285887af30eea15ac3961c5ad0fe41
Frank Casanova <frankcasanova.info@gmail.com>
2021-07-03 16:59:26 +0200

    statics corrections

--- bookmarks/production.py
+++ bookmarks/production.py
@@ -157,8 +157,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'images.static')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICTILES_DIRS = [os.path.join(BASE_DIR, 'images/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
